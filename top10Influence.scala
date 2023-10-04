a4739f9f1dd864fa840031765fd2aa967c8ae9d4
Nimishmangee <82581324+Nimishmangee@users.noreply.github.com>
2023-10-04 13:49:48 +0530

    second commit

---
+++ top10Influence.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.graphx.GraphLoader
+import org.apache.spark.graphx._
+import org.apache.spark.sql.functions._
+
+val retweetGraph = GraphLoader.edgeListFile(sc, "retweet_network.edgelist")
+val mentionGraph = GraphLoader.edgeListFile(sc, "mention_network.edgelist")
+
+// Combine the retweet and mention graphs by merging their vertices and edges
+val combinedGraph = Graph(
+  retweetGraph.vertices.union(mentionGraph.vertices),
+  retweetGraph.edges.union(mentionGraph.edges)
+).cache()
+
+// Run PageRank on the combined influence network
+val pageRank = combinedGraph.pageRank(0.85).vertices
+
+// Find the top 10 users with the highest PageRank scores
+val top10InfluentialPersons = pageRank.top(10)(Ordering.by(_._2))
+
+// Print the top 10 most influential persons
+println("Top 10 most influential persons:")
+top10InfluentialPersons.foreach { case (userId, pageRankScore) =>
+  println(s"User $userId with PageRank $pageRankScore")	
+}
+
+
+
+
+
